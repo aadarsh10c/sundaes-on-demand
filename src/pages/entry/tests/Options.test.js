790ee6167d55155a09d26d932dbb6f56237a3089
Adarsh Kumar <adarsh010c@gmail.com>
2023-08-30 03:28:26 +0530

    made options test changes to pass test.

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
-test.only("should display all images from server", () => {
+test.only("should display all images from server", async () => {
   render(<Options optionType={"scoops"} />);
 
   //find the images
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   //checck there alt text
   const altText = scoopImages.map((img) => img.alt);
-  expect(altText).toHave(["Chocolate scoop", "Vanilla scoop"]);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });

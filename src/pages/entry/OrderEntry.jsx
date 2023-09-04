c51181548fee66e7d3d420a7dff1f2de649943aa
Adarsh Kumar <adarsh010c@gmail.com>
2023-09-05 00:52:03 +0530

    Added waitFor ,and modified code

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import Options from "./Options";
 
 export default function OrderEntry() {
-  return <div>OrderEntry</div>;
+  return (
+    <div>
+      <Options optionType="scoops" />
+      <Options optionType="toppings" />
+    </div>
+  );
 }
--- src/pages/entry/tests/OrderEntry.test.js
+++ src/pages/entry/tests/OrderEntry.test.js
@@ -1,4 +1,4 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -6,17 +6,18 @@ import { server } from "../../../mocks/server";
 test("should throw error when renderd", async () => {
   server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
-      ctx.status(500);
+      return res(ctx.status(500));
     }),
     rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
-      ctx.status(500);
+      return res(ctx.status(500));
     })
   );
 
   render(<OrderEntry />);
-  const errorList = await screen.findAllByRole("alert", {
-    name: "An unexpected error occured. Please try again later.",
-  });
+  await waitFor(async() => {
+    const errorList = await screen.findAllByRole("alert");
 
-  expect(errorList).toHave(2);
+    expect(errorList).toHaveLength(2);
+  })
+  
 });
